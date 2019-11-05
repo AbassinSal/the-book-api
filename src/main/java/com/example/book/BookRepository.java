d184c5465616352f9bbcd61401da1286e2196ecf
Abassin Saleh <Abassin.saleh@jambit.com>
2019-11-05 14:18:14 +0100

    feature/Book-1 add BookRepository, refactor BookApplication, add dependency

---
+++ src/main/java/com/example/book/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.book;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "book", path = "book")
+public interface BookRepository extends MongoRepository<Book, String> {
+
+}
