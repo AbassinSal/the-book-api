6f482d992e6ca00c57a5ea7104629c4e10f976ee
Abassin Saleh <Abassin.saleh@jambit.com>
2019-11-06 16:50:15 +0100

    feature/Book-5-get_endpoint
    add get-specific-books endpoint,
    add BookNotFoundException

--- src/main/java/com/example/book/BookController.java
+++ src/main/java/com/example/book/BookController.java
@@ -10,7 +10,7 @@ public class BookController {
     BookController(BookRepository repository) {
         this.repository = repository;
     }
-    
+
     @PostMapping("/books")
     Book createNewBook(@RequestBody Book newBook) {
         return repository.save(newBook);
@@ -25,4 +25,58 @@ public class BookController {
     int showNumberOfBooks() {
         return repository.findAll().size();
     }
+
+    @GetMapping("/books/{searchAttribute}/{searchString}")
+    List<Book> showSpecificBook(@PathVariable("searchAttribute") String searchAttribute,
+                                @PathVariable("searchString") String searchString) {
+
+        List<Book> allBooks = repository.findAll();
+        List<Book> filteredBooks = new ArrayList<>();
+
+        int i = 0;
+
+        if (searchAttribute.equalsIgnoreCase("title")) {
+            for (Book book : allBooks) {
+                i++;
+                String currentTitle = book.getTitle();
+                if (currentTitle.equalsIgnoreCase(searchString)) {
+                    filteredBooks.add(book);
+                } else if (i >= allBooks.size() - 1 && filteredBooks.isEmpty()) {
+                    throw new BookNotFoundException(searchString);
+                }
+            }
+        } else if (searchAttribute.equalsIgnoreCase("genre")) {
+            for (Book book : allBooks) {
+                i++;
+                String currentGenre = book.getGenre();
+                if (currentGenre.equalsIgnoreCase(searchString)) {
+                    filteredBooks.add(book);
+                } else if (i >= allBooks.size() - 1 && filteredBooks.isEmpty()) {
+                    throw new BookNotFoundException(searchString);
+                }
+            }
+        } else if (searchAttribute.equalsIgnoreCase("publicationDate")) {
+            for (Book book : allBooks) {
+                i++;
+                String currentPublicationDate = book.getPublicationDate();
+                if (currentPublicationDate.equalsIgnoreCase(searchString)) {
+                    filteredBooks.add(book);
+                } else if (i >= allBooks.size() - 1 && filteredBooks.isEmpty()) {
+                    throw new BookNotFoundException(searchString);
+                }
+            }
+        } else if (searchAttribute.equalsIgnoreCase("author")) {
+            for (Book book : allBooks) {
+                i++;
+                String currentAuthor= book.getAuthor();
+                if (currentAuthor.equalsIgnoreCase(searchString)) {
+                    filteredBooks.add(book);
+                } else if (i >= allBooks.size() - 1 && filteredBooks.isEmpty()) {
+                    throw new BookNotFoundException(searchString);
+                }
+            }
+        }
+        return filteredBooks;
+    }
 }
+
