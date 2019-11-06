7510628c923ca7903b57841925433770d07d0188
Abassin Saleh <Abassin.saleh@jambit.com>
2019-11-06 13:55:43 +0100

    Book-3-create-BookController add RestController

---
+++ src/main/java/com/example/book/BookController.java
@@ -0,0 +1,13 @@
+package com.example.book;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BookController {
+
+    private final BookRepository repository;
+
+    BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+}
