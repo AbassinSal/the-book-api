0e663bbeb4f5741e35e650a5b7ab61344fe7ccc5
Abassin Saleh <Abassin.saleh@jambit.com>
2019-11-11 15:30:20 +0100

    fix/Book-1-refactoring_endpoints
    replace BookRepository param String to param Integer

--- src/main/java/com/example/book/BookNotFoundException.java
+++ src/main/java/com/example/book/BookNotFoundException.java
@@ -2,6 +2,6 @@ package com.example.book;
 
 class BookNotFoundException extends RuntimeException{
     BookNotFoundException(String type, String attribute) {
-        super("Could not find book with  "+ type + ": " + attribute);
+        super("Could not find book with "+ type + ": " + attribute);
     }
 }
